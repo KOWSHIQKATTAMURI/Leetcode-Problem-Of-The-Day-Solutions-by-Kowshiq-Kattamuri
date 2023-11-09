599cf90b67116ffca6e766b5a18e985571494f1f
Kattamuri Kowshiq <kattamuri_b201298bt@nitc.ac.in>
2023-11-09 05:42:59 +0530

    Time: 29 ms (34.13%) | Memory: 11.9 MB (94.78%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int countHomogenous(string s){
         int mod = 1e9 + 7;
-        vector<int> dp(s.length(), 1);
+        int prev = 1;
+        long long ans = prev;
         for(int i = 1; i < s.length(); i++){
+            int curr = 1;
             if(s[i] == s[i - 1]){
-                dp[i] += dp[i - 1];
+                curr += prev;
             }
+            prev = curr;
+            ans += curr;
         }
-        long long sum = accumulate(dp.begin(), dp.end(), 0LL);
-        return sum%mod;
+        return ans%mod;
     }
 };
